15eed284e41f024235017a5d0542d5d60e232529
KoblizekXD <janprokupek@centrum.cz>
2024-01-01 09:16:33 +0100

    chore: update gradle script

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java-library")
+    `maven-publish`
+    `java-gradle-plugin`
 }
 
 group = "lol.koblizek"
@@ -13,6 +15,19 @@ dependencies {
     implementation("org.slf4j:slf4j-api:2.0.9")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = "lol.koblizek"
+            artifactId = "inter-api"
+            from(components["java"])
+        }
+    }
+    repositories {
+        mavenLocal()
+    }
 }
 
 tasks.test {
