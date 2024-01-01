107c0acb7adfcbc57e6ec5b848d3b8a6d7076707
KoblizekXD <janprokupek@centrum.cz>
2024-01-01 09:12:30 +0100

    RuntimeManager abstraction

--- src/main/java/lol/koblizek/inter/client/RuntimeManager.java
+++ src/main/java/lol/koblizek/inter/client/RuntimeManager.java
@@ -1,4 +1,13 @@
 package lol.koblizek.inter.client;
 
+import java.io.File;
+
 public abstract class RuntimeManager {
+    public abstract String getArgs();
+    public abstract void exit();
+    public abstract void exit(int status);
+    public abstract void crash();
+    public abstract File getGameDirectory();
+    public abstract String getVMArguments();
+    public abstract File getVMDirectory();
 }
