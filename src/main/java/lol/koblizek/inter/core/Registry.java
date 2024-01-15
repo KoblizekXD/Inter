6749a425b45b99008efc61a712fa53e8d850c3aa
KoblizekXD <janprokupek@centrum.cz>
2024-01-15 07:40:51 +0100

    feat: registry stuff

--- src/main/java/lol/koblizek/inter/core/Registry.java
+++ src/main/java/lol/koblizek/inter/core/Registry.java
@@ -1,10 +1,17 @@
 package lol.koblizek.inter.core;
 
+import lol.koblizek.inter.Inter;
+
 public abstract class Registry<T extends Identifiable> {
 
     static <E extends Identifiable> void register(Registry<E> registry, E entry) {
-        registry.register(entry);
+        if (!registry.isFrozen())
+            registry.register(entry);
+        else
+            Inter.getSystemLogger().error("Cannot register {} after registry is frozen", entry.getKey().getResource());
     }
 
     abstract void register(T entry);
+    abstract void freeze();
+    abstract boolean isFrozen();
 }
